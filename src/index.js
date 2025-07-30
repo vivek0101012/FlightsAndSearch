32c4f5927fe38d874a945800af36b6d3981a309a
VIVEK NISHAD <vknitk2026@gmail.com>
2025-07-30 23:56:55 +0530

    Setups

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const { PORT } = require('./config/serverConfig');
+const bodyParser = require("body-parser");
 
 
 const setupAndStartServer= async ()=>{
 
 const app= express()
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 app.listen(PORT,()=>{
     console.log(`server started at ${PORT}`)
 })
+
 
 
 
