84315e3fb287a9ee75599d13b4ebd127a267ddd7
VIVEK NISHAD <vknitk2026@gmail.com>
2025-08-06 00:04:31 +0530

    added unique constraints and started the cityRepository

--- src/repositry/city-repository.js
+++ src/repositry/city-repository.js
@@ -1,1 +1,49 @@
-const {City}=require("../models/index")
+
+const { where } = require("sequelize");
+const {City}=require("../models/index");
+
+class CityRepository {
+
+
+    async createcity({name} ){
+
+try {
+const city =await City.create({name});
+
+return city;
+    
+} catch (error) {
+
+    throw {error};
+    
+}
+  }
+
+
+async deleteCity (cityId) { 
+
+try {
+    
+    cit
+
+     await  City.destroy({
+        where:{
+            id:cityId
+        }
+     });
+     
+} catch (error) {
+    
+    throw error;
+}
+
+
+
+}
+
+
+}
+
+
+
+module.exports=CityRepository;
