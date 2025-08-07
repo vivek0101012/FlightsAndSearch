a745c41baac06e5508a789195f07a3d13f455b74
VIVEK NISHAD <vknitk2026@gmail.com>
2025-08-07 12:57:50 +0530

    completed city repository

--- src/repositry/city-repository.js
+++ src/repositry/city-repository.js
@@ -1,46 +1,92 @@
 
 
+const { where } = require("sequelize");
 const {City}=require("../models/index");
 
 class CityRepository {
 
 
-    async createcity({name} ){
+     async createCity({name}){
+        
+        try{
+          const city=await City.create({
+            name
+        });
+ 
+        return city;
+        
+        }
+        
+        catch (error){
+              console.log("something went wrong in the repo layer ")
+              throw {error}
+        
+         }
 
-try {
-const city =await City.create({name});
+     }
 
-return city;
-    
-} catch (error) {
+      
+     async deleteCity (cityId){
+        try {
+            await City.destroy({
+                where:{
+                    id: cityId
+                }
+                
+            })
+            return true ;
+        } catch (error) {
+               console.log("something went wrong in the repo layer ")
+              throw {error}
+            
+        }
 
-    throw {error};
-    
-}
-  }
+     }
 
+     
+     async updateCity(data, cityId){
 
-async deleteCity (cityId) { 
-
-try {
-    
-    
-
-     await  City.destroy({
+     try {
+            const city=await City.update( data,{
         where:{
             id:cityId
         }
-     });
-     
-} catch (error) {
+    } )
+
+    return city;
+          
+
+        } catch (error) {
+
+             console.log("something went wrong in the repo layer ")
+              throw {error}
+            
+            
+        }
+
+
+
+
+     }
+
     
-    throw error;
-}
+     async getCity(cityId){
 
+        try {
+            
+            const city= City.findByPk(cityId);
+            
+            return city;
 
+        } catch (error) {
 
-}
+             console.log("something went wrong in the repo layer ")
+              throw {error}
+            
+            
+        }
 
+     }
 
 }
 
